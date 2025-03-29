6ecade36f7f60790f452c910c6ef3d483a8b4e81
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-29 16:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let counter = document.getElementById("counter");
+let incrementBtn = document.getElementById("incrementBtn");
+
+function changeNumber(){
+	let num = parseInt(counter.textContent);
+	num += 1;
+	counter.textContent = num;
+	alert(num)
+}
+
+incrementBtn.addEventListener("click", changeNumber);
