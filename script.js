fdbe45107fcbd44a2004e74170996f23eb1ee8f3
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-29 16:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let counter = document.getElementById("counter");
 let incrementBtn = document.getElementById("incrementBtn");
+let counter = document.getElementById("counter");
 
 function changeNumber(){
 	let num = parseInt(counter.textContent);
-	num += 1;
+	num+=1;
 	counter.textContent = num;
 	alert(num)
 }
